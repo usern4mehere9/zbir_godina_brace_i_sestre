2f7e63f184663419a709322ff4db6ea452e4fc85
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-10 13:51:25 +0200

    Add files via upload

---
+++ zbir_godina_brace_i_sestre/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zbir_godina_brace_i_sestre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, raz;
+
+            a = int.Parse(Console.ReadLine());
+
+            raz = a - 3;
+
+            if ((raz % 4)> 0)
+            {
+                Console.WriteLine("ne");
+            }
+            else
+            {
+                Console.WriteLine("da");
+            }
+        }
+    }
+}
